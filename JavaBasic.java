6f2e46eb746c737612dfdb634e5de14c0d21de74
zxaman <amankumardk1@gmail.com>
2025-06-05 14:09:45 +0530

    variables

--- JavaBasic.java
+++ JavaBasic.java
@@ -1,6 +1,23 @@
+//public class JavaBasic{
+
+ //   public static void main(String[] args){
+ //       System.out.println("Hello World!");
+//    }
+//}
+
+
+// how to run the java file
+// command 
+// javac JavaBasic.java
+// java JavaBasic
+
+
 public class JavaBasic{
 
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
+
