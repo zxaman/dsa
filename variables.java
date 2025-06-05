1b559697112fda68d7537274cb5242729109dc9e
zxaman <amankumardk1@gmail.com>
2025-06-05 14:45:40 +0530

    2 same variables assigning

--- variables.java
+++ variables.java
@@ -1,6 +1,17 @@
+// public class variables {
+//     public static void main(String[] args){
+//         int a =10;
+//         System.out.println(a);
+//     }
+// }
+//even if you declare the same variable twice with different values, it will not give any error
+// example code 
 public class variables {
     public static void main(String[] args){
         int a =10;
+        
+        System.out.println(a);
+         a =20;
         System.out.println(a);
     }
 }
