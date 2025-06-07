34e3ed6b164be87e9b94019c11bde9226e3efc7d
zxaman <amankumardk1@gmail.com>
2025-06-07 16:57:44 +0530

    Input in java chpater 6 part 9

---
+++ input.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // String input = sc.next();
+        // String input = sc.nextLine();
+        // int input = sc.nextInt();
+        // byte input = sc.nextByte();
+        // float input = sc.nextFloat();
+        // double input = sc.nextDouble();
+        // boolean input = sc.nextBoolean();
+        // short input = sc.nextShort();
+        long input = sc.nextLong();
+        
+        System.out.println(input);
+    }
+}
